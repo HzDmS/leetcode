9b9269a140dc38062da72b94456752abe4e88af0
Zhe He <hezhehz@live.cn>
2019-08-18 22:45:32 +0200

    Treasure Island

--- Treasure_Island/main.py
+++ Treasure_Island/main.py
@@ -1,47 +1,38 @@
+from collections import deque
+
+
 class Solution(object):
 
     def findShortestPath(self, input_map):
 
-        """
-        Parameters
-        ----------
-        input_map: 2D list.
-
-        Returns
-        -------
-        length: int, length of shortest path.
-        """
         m, n = len(input_map), len(input_map[0])
-        visited = [[False for i in range(n)] for j in range(m)]
+        visited, q = set(), deque()
+
         length = 0
-        paths = []
+        q.append((0, 0))
+        visited.add((0, 0))
 
-        def bfs(row, col, length, paths, visited, input_map):
+        while q:
 
-            if row == m or row < 0 or col == n or col < 0 or visited[row][col]:
-                return
+            for _ in range(len(q)):
+                i, j = q.popleft()
 
-            if input_map[row][col] == 'D':
-                return
+                if input_map[i][j] == "X":
+                    return length
 
-            if input_map[row][col] == 'X':
-                paths.append(length)
-                return
+                for direction in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
 
-            visited[row][col] = True
-            bfs(row + 1, col, length + 1, paths, visited, input_map)
-            bfs(row - 1, col, length + 1, paths, visited, input_map)
-            bfs(row, col + 1, length + 1, paths, visited, input_map)
-            bfs(row, col - 1, length + 1, paths, visited, input_map)
-            visited[row][col] = False
+                    new_i, new_j = i + direction[0], j + direction[1]
 
-        bfs(0, 0, length, paths, visited, input_map)
+                    if new_i >= 0 and new_i < m and new_j >= 0 and new_j < n:
+                        if input_map[new_i][new_j] != "D":
+                            if (new_i, new_j) not in visited:
+                                visited.add((new_i, new_j))
+                                q.append((new_i, new_j))
 
-        shortest = float('inf')
-        for path in paths:
-            shortest = min(shortest, path)
+            length += 1
 
-        return shortest
+        return length
 
 
 if __name__ == "__main__":
