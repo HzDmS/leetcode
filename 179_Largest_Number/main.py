8cdfef782c38b2674395d3f901697834db9c7724
Zhe He <hezhehz@live.cn>
2020-03-23 21:01:00 +0100

    179

---
+++ 179_Largest_Number/main.py
@@ -0,0 +1,9 @@
+from functools import cmp_to_key
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = sorted(nums, key=cmp_to_key(lambda x, y: 1 if str(x) + str(y) < str(y) + str(x) else -1))
+        if nums[0] == 0:
+            return "0"
+        return "".join(list(map(str, nums)))
+
